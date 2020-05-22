42a1cf304393b227f7e6efd3a01743cbdeab77c3
ryand67 <rwdevenney@gmail.com>
2020-05-22 17:28:40 -0400

    Basic questions set up

--- index.js
+++ index.js
@@ -1,3 +1,57 @@
+var genMD = require("./generateMarkdown");
+var fs = require("fs");
+var inquirer = require("inquirer");
+
+inquirer.prompt([{
+        type: "input",
+        message: "Title:",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Description:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Title of Contents",
+        name: "tableOfContents"
+    },
+    {
+        type: "input",
+        message: "Installation:",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Usage:",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Lisence:",
+        name: "lisence"
+    },
+    {
+        type: "input",
+        message: "Contributers:",
+        name: "contributers"
+    },
+    {
+        type: "input",
+        message: "Tests:",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "GitHub Email:",
+        name: "gitEmail"
+    }
+    
+]).then((answers) => {
+    console.log(answers);
+})
+
 const questions = [
 
 ];
